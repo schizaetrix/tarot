cd82ce159673d515a5732bb41e679edd8e725c61
Chiza Barber <chiza.alba@gmail.com>
2019-03-05 13:26:31 -0600

    first routes and components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Heading from './Heading'
+import Landing from './Landing'
+import DailyCard from './DailyCard'
 
 
 class App extends Component {
     render () {
         return (
-            <div>App</div>
+            <div>
+                <Heading />
+                <BrowserRouter>
+                    <div>
+                        <Route 
+                            path='/'
+                            exact component={Landing}
+                        />
+                        <Route 
+                            path='/dailycard'
+                            exact component={DailyCard}
+                        />
+                    </div>
+                </BrowserRouter>
+            </div>
         )
     }
 }
---
+++ client/src/components/DailyCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const DailyCard = () => {
+    return (
+        <div>
+            DailyCard
+        </div>
+    )
+}
+
+export default DailyCard
